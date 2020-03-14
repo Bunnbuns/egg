960289651f9bf85104d84d4a1f421ac634d4b456
easrng <easrng@gmail.com>
2020-03-14 12:01:07 -0400

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,7 +1,7 @@
-function $(id){  //less typing
-    if(id.startsWith(".")){
+function $(id) { //less typing
+    if (id.startsWith(".")) {
         return document.getElementsByClassName(id.substring(1));
-    }else{
+    } else {
         return document.getElementById(id);
     }
 }
@@ -11,46 +11,43 @@ var devMode = false;
 
 var videoLoaded = false;
 
-function showHide(id, display){
-    $(id).style.display = display;
+function showHide(id, display) {
+    $(id)
+        .style
+        .display = display;
 }
 
-function loadVideo(){
-     return new Promise(function(resolve){
-         if(videoLoaded){
-             resolve()
-         }else{
-             $('eggVideo').oncanplay = function() {
-                 resolve();
-             };
-         }
-     })
+function loadVideo() {
+    return new Promise(function (resolve) {
+        var b = setInterval(() => {
+            if (VideoElement.readyState >= 3) {
+                //stop checking every half second
+                clearInterval(b);
+                resolve()
+            }
+        }, 50);
+    })
 }
 
-async function giveEgg(){
+async function giveEgg() {
     await loadVideo();
     showHide('eggLoader', 'none');
     showHide('video', 'block');
     $('eggVideo').play();
 }
 
-function sendEgg(){
+function sendEgg() {
     console.log("sharing egg...");
-    navigator.share({
-        text: '🥚 Get a 🆓 egg! 🥚',
-        url: location.href,
-    });
+    navigator.share({text: '🥚 Get a 🆓 egg! 🥚', url: location.href});
 }
 
-function loadEgg(){
+function loadEgg() {
     console.log('giving egg to user');
     showHide('top', 'none');
     showHide('eggTime', '');
     //show loading spinner for about 1 sec
     showHide('eggLoader', 'block');
-    setTimeout(function() { giveEgg(); }, 350);
+    setTimeout(function () {
+        giveEgg();
+    }, 350);
 }
-
-$('eggVideo').oncanplay = function() {
-    videoLoaded = true;
-};
